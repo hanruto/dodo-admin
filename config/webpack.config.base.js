cf2bda94d7a7d6dc400fec9b12c0111375374123
qm <1256790127@qq.com>
2019-02-23 14:08:30 +0800

    f

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,42 +1,53 @@
 const path = require('path'),
-  HtmlWebpackPlugin = require('html-webpack-plugin')
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  HappyPack = require('happypack'),
+  os = require('os') // node 提供的系统操作模块
 
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.jsx'],
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'main.js',
+    filename: 'main.js'
   },
   resolve: {
-    extensions: ['.jsx', '.js'],
+    extensions: ['.jsx', '.js']
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(js|jsx)$/,
         use: ['babel-loader', { loader: 'eslint-loader', options: { fix: true } }],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/,
-        use: ['url-loader'],
-      },
-    ],
+        use: ['url-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'just dodo',
-      favicon: './favicon.ico',
+      favicon: './favicon.ico'
     }),
-  ],
+    new HappyPack({
+      // 基础参数设置
+      id: 'babel', // 上面loader?后面指定的id
+      loaders: ['babel-loader?cacheDirectory'], // 实际匹配处理的loader
+      threadPool: happyThreadPool,
+      // cache: true // 已被弃用
+      verbose: true
+    })
+  ]
 }
