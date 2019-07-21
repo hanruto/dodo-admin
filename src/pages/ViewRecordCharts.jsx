f824968fce431accb30a1ae697c08bf5dd1065ae
qiaoming <1256790127@qq.com>
2019-07-21 14:19:46 +0800

    f

--- src/pages/ViewRecordCharts.jsx
+++ src/pages/ViewRecordCharts.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-
-import { LineChart, Tooltip, XAxis, CartesianGrid, Line } from 'recharts'
+import { LineChart, Tooltip, XAxis, CartesianGrid, Line, Legend } from 'recharts'
 
 export function Chart(props) {
-  const width = props.data.length * 40
+  const width = props.data.length * 60
 
   return (
     <LineChart width={width} height={450} data={props.data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
-      <XAxis dataKey="label" />
+      <XAxis dataKey="date" />
       <Tooltip />
       <CartesianGrid stroke="#f5f5f5" />
       <Line type="monotone" dataKey="pv" stroke="#3399ff" yAxisId={0} />
+      <Line type="monotone" dataKey="uv" stroke="#ff9933" yAxisId={0} />
+      <Legend />
     </LineChart>
   )
 }
@@ -23,16 +24,16 @@ export default class ViewRecordList extends React.Component {
     this.props.viewRecordStore.getAnalysis()
   }
 
-  get analysis() {
-    const analysis = this.props.viewRecordStore.analysis
-    return analysis.map(item => ({ label: item.date, pv: item.viewCount }))
-  }
-
   render() {
+    const analysis = this.props.viewRecordStore.analysis
+
     return (
-      <div className="do-container">
-        <div className="view-record-chart-wrapper">
-          <Chart data={this.analysis} />
+      <div className="do-container view-record-chart-page">
+        <div className="view-record-chart-card">
+          <h4>访问量统计</h4>
+          <div className="view-record-chart-wrapper">
+            <Chart data={analysis} />
+          </div>
         </div>
       </div>
     )
