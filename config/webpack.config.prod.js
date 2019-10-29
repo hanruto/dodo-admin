05a47ea96223e65dc94c4134c842d2d884db346c
qiaoming <qiaoming@guanghe.tv>
2019-10-29 17:59:12 +0800

    fix: 样式调整

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,9 +1,35 @@
 const baseConfig = require('./webpack.config.base')
 
-
 const config = Object.assign(baseConfig, {
   mode: 'production',
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        tool: {
+          name: 'tool',
+          test: module => /lodash|dayjs|axios/.test(module.context),
+          priority: 11,
+          chunks: 'all',
+          enforce: true
+        },
+        react: {
+          name: 'react',
+          test: module => /react|redux/.test(module.context), // 打包react和redux相关的一些第三方包
+          chunks: 'initial',
+          priority: 10,
+          enforce: true
+        },
+        vender: {
+          name: 'vender',
+          test: module => /node_modules/.test(module.context),
+          chunks: 'initial',
+          priority: 9,
+          enforce: true
+        }
+      }
+    }
+  }
 })
-
 
 module.exports = config
